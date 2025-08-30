d06d7a022e21375192ed0fc514e8830ba80717a0
schaskia <saskia.schneg@gmail.com>
2025-08-29 23:56:52 -0400

    added rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,37 @@
-module.exports = function (eleventyConfig) {
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
+function xmlEscape(str) {
+  if (!str) return "";
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(pluginRss);
+
+    // Register custom filters for Nunjucks
+    eleventyConfig.addNunjucksFilter("xmlEscape", xmlEscape);
+        
     // Passthrough copy for static assets
     eleventyConfig.addPassthroughCopy('src/assets');
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/js');
 
     eleventyConfig.addFilter("limit", function (arr, limit) {
-    return arr.slice(0, limit);
+        return arr.slice(0, limit);
     });
-      // Shuffle / randomize array 
-    eleventyConfig.addFilter("shuffle", function(array) {
-        // Create a copy of the array to avoid modifying the original
-        let shuffledArray = array.slice();
 
-        // Fisher-Yates shuffle algorithm
+    // Shuffle / randomize array 
+    eleventyConfig.addFilter("shuffle", function(array) {
+        let shuffledArray = array.slice();
         for (let i = shuffledArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
         }
-
         return shuffledArray;
     });
 
@@ -28,11 +41,9 @@ module.exports = function (eleventyConfig) {
             output: 'public',
             includes: '_includes',
         },
-
-    templateFormats: ["njk", "md", "html"],
+        templateFormats: ["njk", "md", "html"],
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
         dataTemplateEngine: "njk",
-  };
-
+    };
 };
